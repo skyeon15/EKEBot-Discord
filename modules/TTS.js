1776daedbf58f75cd41d8885af37f2d2b0127c44
에케EKE <eke@bbforest.net>
2022-04-19 14:59:46 +0900

    try

--- modules/TTS.js
+++ modules/TTS.js
@@ -3,23 +3,27 @@ const { AudioPlayer, createAudioResource, StreamType, entersState, VoiceConnecti
 
 module.exports = {
     async execute(message) {
-        let voiceConnection;
-        let audioPlayer = new AudioPlayer();
+        try {
+            let voiceConnection;
+            let audioPlayer = new AudioPlayer();
 
-        const stream = discordTTS.getVoiceStream(message.content, { lang: 'ko' });
-        const audioResource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
-        if (!voiceConnection || voiceConnection?.status === VoiceConnectionStatus.Disconnected) {
-            voiceConnection = joinVoiceChannel({
-                channelId: message.member.voice.channelId,
-                guildId: message.guildId,
-                adapterCreator: message.guild.voiceAdapterCreator,
-            });
-            voiceConnection = await entersState(voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
-        }
+            const stream = discordTTS.getVoiceStream(message.content, { lang: 'ko' });
+            const audioResource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+            if (!voiceConnection || voiceConnection?.status === VoiceConnectionStatus.Disconnected) {
+                voiceConnection = joinVoiceChannel({
+                    channelId: message.member.voice.channelId,
+                    guildId: message.guildId,
+                    adapterCreator: message.guild.voiceAdapterCreator,
+                });
+                voiceConnection = await entersState(voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
+            }
 
-        if (voiceConnection.status === VoiceConnectionStatus.Connected) {
-            voiceConnection.subscribe(audioPlayer);
-            audioPlayer.play(audioResource);
+            if (voiceConnection.status === VoiceConnectionStatus.Connected) {
+                voiceConnection.subscribe(audioPlayer);
+                audioPlayer.play(audioResource);
+            }
+        } catch (error) {
+            return
         }
     }
 };
