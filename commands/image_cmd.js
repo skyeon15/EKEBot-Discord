0373473faaf0053eab2cd9d6d1ec21d9ef15a99b
에케EKE <eke@bbforest.net>
2023-03-26 23:54:53 +0900

    오류수정

--- commands/image_cmd.js
+++ commands/image_cmd.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const Chat = require('../modules/openai')
+const Chat = require('../modules/openAi')
 
 module.exports = {
   data: new SlashCommandBuilder()
