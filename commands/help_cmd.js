eb95c3ab818a2d0fd374d5a34947b9a4846b2241
에케EKE <eke@bbforest.net>
2023-03-26 23:52:46 +0900

    오류수정

--- commands/help_cmd.js
+++ commands/help_cmd.js
@@ -14,9 +14,11 @@ module.exports = {
             .addField('핑', '봇 지연시간을 확인해요.')
             .addField('코로나', '대한민국 코로나19 상황을 알려줘요.')
             .addField('말 <할말>', '문장을 입력하면 대신 말해줘요.')
+            .addField('에케야 <할말>', '에케봇이랑 대화해요')
             .addField('시세 <금/기름/코인> [코인심볼]', '각종 시세를 알려줘요.')
             .addField('급식 <학교이름>', '학교 급식을 알려줘요.')
             .addField('번역 <출발언어> <도착언어> <내용>', '입력한 문장을 번역해줘요.')
+            .addField('에케채널 <tts/translate/chat>', '에케봇 전용 채널을 설정해요.')
             .setFooter({ text: '에케봇 By.파란대나무숲', iconURL: 'https://i.imgur.com/fWGVv2K.png' });
 
         interaction.reply({ embeds: [Embed] });
